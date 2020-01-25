91afc2e07a0f0786d69474179e140a2f77b5086e
pinstripeninjas <pinstripeninjas@gmail.com>
2020-01-25 11:19:57 -0700

    img object created

--- js/script.js
+++ js/script.js
@@ -1,3 +1,60 @@
+///////////////////////////////////////////////////////////
+// List of buttons with image names and locations
+///////////////////////////////////////////////////////////
+const buttons = [
+	// start of button one code
+	{
+		buttonTitle: "Button One",
+		buttonItems: [
+			// image one
+			{
+				imgName: "Image One",
+				imgLocation: "https://picsum.photos/400"
+			},
+			// image two
+			{
+				imgName: "Image Two",
+				imgLocation: "https://picsum.photos/400"
+			},
+			//image three
+			{
+				imgName: "Image Three",
+				imgLocation: "https://picsum.photos/400"
+			}
+			// end of images
+		]
+	},
+	// end of button one code
+
+	// start of button two code
+	{
+		buttonTitle: "Button Two",
+		buttonItems: [
+			// image one
+			{
+				imgName: "Image One",
+				imgLocation: "https://picsum.photos/400"
+			},
+			// image two
+			{
+				imgName: "Image Two",
+				imgLocation: "https://picsum.photos/400"
+			},
+			//image three
+			{
+				imgName: "Image Three",
+				imgLocation: "https://picsum.photos/400"
+			}
+			// end of images
+		]
+	}
+	// end of button two code
+];
+
+///////////////////////////////////////////////////////////
+// Top secret code, don't look below here...or else
+///////////////////////////////////////////////////////////
+
 const btnListener = document.querySelectorAll(".dropdown-item");
 
 for (item of btnListener) {
